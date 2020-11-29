84b0047901ac199eb78fe6b5f029e484a95fb63e
Burhanuddin Udaipurwala <udaipurwalaburhanuddin@gmail.com>
2020-11-29 12:41:32 +0530

    Modify client to use query service

--- blog-app/client/src/components/CommentList.js
+++ blog-app/client/src/components/CommentList.js
@@ -1,21 +1,4 @@
-import Axios from "axios";
-import { useEffect, useState } from "react";
-
-const CommentList = ({ postId }) => {
-  const [comments, setComments] = useState([]);
-
-  const fetchComments = async () => {
-    const response = await Axios.get(
-      `http://localhost:4001/posts/${postId}/comments`
-    );
-
-    setComments(response.data);
-  };
-
-  useEffect(() => {
-    fetchComments();
-  }, []);
-
+const CommentList = ({ comments }) => {
   const renderedComments = comments.map((comment) => (
     <li key={comment.id}>{comment.content}</li>
   ));
--- blog-app/client/src/components/PostList.js
+++ blog-app/client/src/components/PostList.js
@@ -7,7 +7,7 @@ const PostList = () => {
   const [posts, setPosts] = useState({});
 
   const fetchPosts = async () => {
-    const response = await Axios("http://localhost:4000/posts");
+    const response = await Axios("http://localhost:4002/posts");
     const posts = response.data;
     setPosts(posts);
   };
@@ -24,7 +24,7 @@ const PostList = () => {
     >
       <div className="card-body">
         <h3>{post.title}</h3>
-        <CommentList postId={post.id} />
+        <CommentList comments={post.comments} />
         <CommentCreate postId={post.id} />
       </div>
     </div>
